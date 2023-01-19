2d0ca68f0b2c95077d3dc1b4e0cf01bd0f4a0cc6
taylorwood95 <115211674+taylorwood95@users.noreply.github.com>
2023-01-19 15:12:51 +0000

    main branch

---
+++ src/compare.py
@@ -0,0 +1,9 @@
+def compare(number_1, number_2):
+    if number_1 < number_2:
+        return f"{number_1} is less than {number_2}"
+
+    elif number_1 == number_2:
+        return f"{number_1} is the same as {number_2}"
+    else:
+
+        return f"{number_1} is greater than {number_2}"
